7ec72024e2035e9af30a7959dedf4723a745f87b
DelioCoder <davideveriwhere@gmail.com>
2025-06-09 15:44:38 -0500

    Translation component added

---
+++ utils/translation.ts
@@ -0,0 +1,9 @@
+export const translations: Record<string, string> = {
+  keyboard: "teclado",
+  mouse: "ratón",
+  tv: "televisor",
+  person: "persona",
+  chair: "silla",
+  table: "mesa",
+  // agrega más según tu modelo
+};
