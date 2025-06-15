7ed88b99bf2d3759828ac71bf87d92a2e607dc8a
DelioCoder <davideveriwhere@gmail.com>
2025-06-15 17:45:49 -0500

    Speech functionality splitted

--- app/index.tsx
+++ app/index.tsx
@@ -1,69 +1,25 @@
 import CameraComponent, { CameraComponentHandle } from '@/components/CameraComponent';
-import { useDetectObjects } from '@/hooks/useUploadImage';
-import { translations } from '@/utils/translation';
+import SpeechComponent from '@/components/SpeechComponent';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import Octicons from '@expo/vector-icons/Octicons';
 import { CameraType } from 'expo-camera';
-import * as Speech from 'expo-speech';
 import {
   ExpoSpeechRecognitionModule,
-  useSpeechRecognitionEvent,
 } from "expo-speech-recognition";
 import React, { useRef, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Home() {
   const [facing, setFacing] = useState<CameraType>('back');
-  const [recognizing, setRecognizing] = useState<boolean>(false);
   const [isListening, setIsListening] = useState<boolean>(false);
+  const [recognizing, setRecognizing] = useState<boolean>(false);
+
   const [detectedLabels, setDetectedLabels] = useState<string[]>([]);
   const cameraRef = useRef<CameraComponentHandle>(null);
-
-  const { detect } = useDetectObjects();
 
   function toggleCameraFacing() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
-
-  useSpeechRecognitionEvent('start', () => { setRecognizing(true); setIsListening(true) });
-  useSpeechRecognitionEvent('end', () => { setRecognizing(false); setIsListening(false) });
-  useSpeechRecognitionEvent('result', async (event) => {
-    const text = event.results[0]?.transcript?.toLowerCase() || '';
-
-    console.log('Texto reconocido:', text);
-
-    if (text.includes('dave') && text.includes('qué ves al frente')) {
-      console.log('Frase detectada: Dave, ¿qué ves al frente mío?');
-      ExpoSpeechRecognitionModule.stop();
-      const uri = await cameraRef.current?.takePhoto();
-      if (uri) {
-        try {
-          Speech.speak('Procesando imagen, por favor espera...', {
-            language: 'es-PE',
-            rate: 1.0,
-          });
-          const response = await detect(uri);
-          const labels = response.objects.map((object) => translations[object.label] || object.label);
-          setDetectedLabels(labels);
-
-          const spokenText = labels.length > 0
-            ? `He detectado: ${labels.join(', ')}`
-            : 'No he detectado ningún objeto';
-
-          Speech.speak(spokenText, {
-            language: 'es-PE',
-            rate: 1.0,
-          });
-        } catch (error) {
-          console.error('Error al detectar objetos:', error);
-          Speech.speak('Hubo un error al analizar la imagen', { language: 'es-PE' });
-        }
-      }
-    }
-  });
-  useSpeechRecognitionEvent('error', (event) => {
-    console.log('Error code:', event.error, 'message:', event.message);
-  });
 
   const handleStart = async () => {
     const result = await ExpoSpeechRecognitionModule.requestPermissionsAsync();
@@ -84,6 +40,7 @@ export default function Home() {
 
   return (
     <View style={styles.container}>
+      <SpeechComponent cameraRef={cameraRef} setDetectedLabels={setDetectedLabels} setIsListening={setIsListening} setRecognizing={setRecognizing} />
       <CameraComponent ref={cameraRef} toggleCameraFacing={toggleCameraFacing} facing={facing} />
 
       <TouchableOpacity onPress={toggleCameraFacing} style={styles.flipButton}>
---
+++ components/SpeechComponent.tsx
@@ -0,0 +1,64 @@
+import { useDetectObjects } from '@/hooks/useUploadImage';
+import { translations } from '@/utils/translation';
+import * as Speech from 'expo-speech';
+import {
+    ExpoSpeechRecognitionModule,
+    useSpeechRecognitionEvent,
+} from "expo-speech-recognition";
+import React from 'react';
+import { CameraComponentHandle } from './CameraComponent';
+interface Props {
+    setRecognizing: React.Dispatch<React.SetStateAction<boolean>>;
+    setIsListening: React.Dispatch<React.SetStateAction<boolean>>;
+    cameraRef: React.RefObject<CameraComponentHandle | null>;
+    setDetectedLabels: React.Dispatch<React.SetStateAction<string[]>>;
+}
+const SpeechComponent = ({ setRecognizing, setIsListening, cameraRef, setDetectedLabels }: Props) => {
+
+
+    const { detect } = useDetectObjects();
+
+    useSpeechRecognitionEvent('start', () => { setRecognizing(true); setIsListening(true) });
+    useSpeechRecognitionEvent('end', () => { setRecognizing(false); setIsListening(false) });
+    useSpeechRecognitionEvent('result', async (event) => {
+        const text = event.results[0]?.transcript?.toLowerCase() || '';
+
+        console.log('Texto reconocido:', text);
+
+        if (text.includes('dave') && text.includes('qué ves al frente')) {
+            console.log('Frase detectada: Dave, ¿qué ves al frente mío?');
+            ExpoSpeechRecognitionModule.stop();
+            const uri = await cameraRef.current?.takePhoto();
+            if (uri) {
+                try {
+                    Speech.speak('Procesando imagen, por favor espera...', {
+                        language: 'es-PE',
+                        rate: 1.0,
+                    });
+                    const response = await detect(uri);
+                    const labels = response.objects.map((object) => translations[object.label] || object.label);
+                    setDetectedLabels(labels);
+
+                    const spokenText = labels.length > 0
+                        ? `He detectado: ${labels.join(', ')}`
+                        : 'No he detectado ningún objeto';
+
+                    Speech.speak(spokenText, {
+                        language: 'es-PE',
+                        rate: 1.0,
+                    });
+                } catch (error) {
+                    console.error('Error al detectar objetos:', error);
+                    Speech.speak('Hubo un error al analizar la imagen', { language: 'es-PE' });
+                }
+            }
+        }
+    });
+    useSpeechRecognitionEvent('error', (event) => {
+        console.log('Error code:', event.error, 'message:', event.message);
+    });
+
+    return null;
+}
+
+export default SpeechComponent;
